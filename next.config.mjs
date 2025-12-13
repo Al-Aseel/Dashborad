5a15751121ab9677629f97c1f7e30f09f8c03f10
mahmoud <al.gaafar200@gmail.com>
2025-12-13 12:18:51 +0200

    new

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,28 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.elaseel.org',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'elaseel.org',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
